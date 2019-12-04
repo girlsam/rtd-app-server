f14a7fc27d275776de8f9c8b466273ddd0d7443a
Sam Goldsmith <samggold@gmail.com>
2019-12-03 17:12:57 -0700

    Attempts to get call working with gtfs

---
+++ src/datasources/RTDFeedAPI.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import { RESTDataSource } from 'apollo-datasource-rest';
+import { GtfsRealtimeBindings } from 'gtfs-realtime-bindings';
+
+export default class RTDFeedAPI extends RESTDataSource {
+  constructor() {
+    super();
+    this.baseURL = process.env.RTD_FEED_URL;
+  }
+
+  async getData() {
+    const res = await axios.get(`${this.baseURL}/VehiclePosition.pb`, { 
+      withCredentials: true,
+      auth: {
+        username: process.env.RTD_FEED_USERNAME,
+        password: process.env.RTD_FEED_PASSWORD
+      }
+    });
+
+    return GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(res.data);
+  }
+}
